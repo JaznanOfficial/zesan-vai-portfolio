b03c3c3abad1a7dd102edd6c59df4ccd31785e5e
JaznanOfficial <jaznanofficial@gmail.com>
2023-05-24 18:49:01 +0600

    page all done

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,167 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import mac from "/src/assets/images/apexmarket mac.png";
+import car from "/src/assets/images/car-inner.png";
+import mobile from "/src/assets/images/mobile.png";
+import ideateui from "/src/assets/images/ideate-ui.png";
+import mobile2 from "/src/assets/images/mobile-2.png";
+import ideateicon from "/src/assets/images/ideate-icons.png";
+import mobile3 from "/src/assets/images/mobile-3.png";
+
+const Projects = () => {
+    return (
+        <div>
+            <div className="relative border md:border-0 rounded-md my-3">
+                <div className="">
+                    <Image loading="lazy" src={mac} alt="mac" width={1600} height={1240} />
+                </div>
+                <div className="md:absolute md:bottom-1 md:left-12 p-3 ">
+                    <p className="md:text-white text-[16px] text-center md:text-left">
+                        Finance, Fintech, Website, Saas, Admin, Ux Design
+                    </p>
+                    <h3 className="md:text-white text-[28px] font-[600] text-center md:text-left">
+                        Apex Markets - Client and Admin
+                    </h3>
+                    <div className="flex flex-row justify-center items-center md:justify-start md:items-start gap-5 mt-5">
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Case Study
+                        </button>
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Live Site
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="relative border md:border-0 rounded-md my-3">
+                <div className="">
+                    <Image loading="lazy" src={car} alt="mac" width={1600} height={1240} />
+                </div>
+                <div className="md:absolute md:bottom-1 md:left-12 p-3 ">
+                    <p className="md:text-white text-[16px] text-center md:text-left">
+                        Finance, Fintech, Website, Saas, Admin, Ux Design
+                    </p>
+                    <h3 className="md:text-white text-[28px] font-[600] text-center md:text-left">
+                        Apex Markets - Client and Admin
+                    </h3>
+                    <div className="flex flex-row justify-center items-center md:justify-start md:items-start gap-5 mt-5">
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Case Study
+                        </button>
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Live Site
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="relative border md:border-0 rounded-md my-3">
+                <div className="">
+                    <Image loading="lazy" src={mobile} alt="mac" width={1600} height={1240} />
+                </div>
+                <div className="md:absolute md:bottom-1 md:left-12 p-3 ">
+                    <p className="md:text-black text-[16px] text-center md:text-left">
+                        Finance, Fintech, Website, Saas, Admin, Ux Design
+                    </p>
+                    <h3 className="md:text-black text-[28px] font-[600] text-center md:text-left">
+                        Apex Markets - Client and Admin
+                    </h3>
+                    <div className="flex flex-row justify-center items-center md:justify-start md:items-start gap-5 mt-5">
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Case Study
+                        </button>
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Live Site
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="relative border md:border-0 rounded-md my-3">
+                <div className="">
+                    <Image loading="lazy" src={ideateui} alt="mac" width={1600} height={1240} />
+                </div>
+                <div className="md:absolute md:bottom-1 md:left-12 p-3 ">
+                    <p className="md:text-white text-[16px] text-center md:text-left">
+                        Finance, Fintech, Website, Saas, Admin, Ux Design
+                    </p>
+                    <h3 className="md:text-white text-[28px] font-[600] text-center md:text-left">
+                        Apex Markets - Client and Admin
+                    </h3>
+                    <div className="flex flex-row justify-center items-center md:justify-start md:items-start gap-5 mt-5">
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Case Study
+                        </button>
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Live Site
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="relative border md:border-0 rounded-md my-3">
+                <div className="">
+                    <Image loading="lazy" src={mobile2} alt="mac" width={1600} height={1240} />
+                </div>
+                <div className="md:absolute md:bottom-1 md:left-12 p-3 ">
+                    <p className="md:text-black text-[16px] text-center md:text-left">
+                        Finance, Fintech, Website, Saas, Admin, Ux Design
+                    </p>
+                    <h3 className="md:text-black text-[28px] font-[600] text-center md:text-left">
+                        Apex Markets - Client and Admin
+                    </h3>
+                    <div className="flex flex-row justify-center items-center md:justify-start md:items-start gap-5 mt-5">
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Case Study
+                        </button>
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Live Site
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="relative border md:border-0 rounded-md my-3">
+                <div className="">
+                    <Image loading="lazy" src={ideateicon} alt="mac" width={1600} height={1240} />
+                </div>
+                <div className="md:absolute md:bottom-1 md:left-12 p-3 ">
+                    <p className="md:text-white text-[16px] text-center md:text-left">
+                        Finance, Fintech, Website, Saas, Admin, Ux Design
+                    </p>
+                    <h3 className="md:text-white text-[28px] font-[600] text-center md:text-left">
+                        Apex Markets - Client and Admin
+                    </h3>
+                    <div className="flex flex-row justify-center items-center md:justify-start md:items-start gap-5 mt-5">
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Case Study
+                        </button>
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Live Site
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="relative border md:border-0 rounded-md my-3">
+                <div className="">
+                    <Image loading="lazy" src={mobile3} alt="mac" width={1600} height={1240} />
+                </div>
+                <div className="md:absolute md:bottom-1 md:left-12 p-3 ">
+                    <p className="md:text-black text-[16px] text-center md:text-left">
+                        Finance, Fintech, Website, Saas, Admin, Ux Design
+                    </p>
+                    <h3 className="md:text-black text-[28px] font-[600] text-center md:text-left">
+                        Apex Markets - Client and Admin
+                    </h3>
+                    <div className="flex flex-row justify-center items-center md:justify-start md:items-start gap-5 mt-5">
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Case Study
+                        </button>
+                        <button className="border-2 border-white text-[17px] text-white mx-1 md:mx-0 md:py-3 py-2 rounded-3xl bg-black md:px-12 px-5 cursor-pointer">
+                            Live Site
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
